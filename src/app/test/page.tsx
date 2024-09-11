43af4f97ca1a8ecfded821d54081135bbcf39f82
AalyTokombaev <alexiver96@hotmail.com>
2024-09-11 13:01:31 +0200

    feat: navigation content v1

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,7 @@
 export default function MyTest() {
-    return (
-        <div>
-            Wallah bror
-        </div>
-    )
+  return (
+    <div>
+      Wallah bror
+    </div>
+  )
 }
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,7 +1,11 @@
 export function Navigation() {
   return (
-    <div className="flex flex row gap-2">
-      <div></div>
+    <div className="flex flex row gap-2 text-lg w-full">
+      <div>
+        <div>Home</div>
+        <div>About</div>
+        <div>Fun stuff!</div>
+      </div>
     </div>
   )
 }
