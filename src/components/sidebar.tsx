2c52bd30a50e402ba5d83769260e5d249a329eb1
AalyTokombaev <alexiver96@hotmail.com>
2024-09-11 13:37:07 +0200

    feat: basic sidebar

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,10 @@
+export function Sidebar() {
+  return (
+    <div className="flex flex-col gap-2">
+      <span>Welcome</span>
+      <span>to</span>
+      <span>my</span>
+      <span>haus</span>
+    </div>
+  )
+}
