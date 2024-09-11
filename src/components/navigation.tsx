429911de692c32db54e01e6f0e2074e5d26d04d5
AalyTokombaev <alexiver96@hotmail.com>
2024-09-11 13:51:05 +0200

    fix: center nav

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -2,19 +2,32 @@
 import { useRouter } from "next/navigation"
 
 export function Navigation() {
-  const router = useRouter()
 
   return (
-    <div className="text-lg w-full justify-center">
-      <div className="flex flex-row gap-2 mx-20">
-        <a
-          className="bg-yellow-400 border-2 border red-500 text-red-500 hover:animate-bounce hover:animate-recoloring"
-        >
-          Home
-        </a>
-        <a>About</a>
-        <a>Fun stuff!</a>
+    <div className="text-lg w-full justify-center bg-lime-200 m-auto">
+      <div className="flex flex-row gap-2 m-auto w-fit">
+        <NavItem name="Home" />
+        <NavItem name="About" />
+        <NavItem name="Funny stuff" />
       </div>
     </div>
+  )
+}
+
+type NavItemsProps = {
+  name: string
+  dist?: string
+}
+function NavItem({ name, dist }: NavItemsProps) {
+
+  const router = useRouter()
+  function handleClick() { }
+
+  return (
+    <a
+      className="bg-yellow-40 border-2 border red-500 text-red-500 hover:animate-bounce hover:animate-recoloring"
+    >
+      {name}
+    </a>
   )
 }
