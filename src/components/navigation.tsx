c978cc62a3284e9b6f49976f66dd68918b5a011e
Andreas Kvernhaug <kvernhaug@hotmail.no>
2024-09-22 15:01:07 +0200

    Fixed pointer cursor on navitem hover

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -6,7 +6,7 @@ export function Navigation() {
   return (
     <div className="text-lg w-full justify-center bg-lime-200 m-auto">
       <div className="flex flex-row gap-2 m-auto w-fit">
-        <NavItem name="Home" />
+        <NavItem name="Home" dist="/"/>
         <NavItem name="About" />
         <NavItem name="Funny stuff" />
         <NavItem name="My Links" dist="mylinks"/>
@@ -23,15 +23,13 @@ function NavItem({ name, dist }: NavItemsProps) {
 
   const router = useRouter()
   function handleClick() { 
-    router.push(
-      dist??""
-    )
+    router.push(dist??"")
    }
 
   return (
     <a
       onClick={handleClick}
-      className="bg-yellow-40 border-2 border red-500 text-red-500 hover:animate-bounce hover:animate-recoloring"
+      className="bg-yellow-40 border-2 border red-500 text-red-500 hover:animate-bounce hover:animate-recoloring cursor-pointer"
     >
       {name}
     </a>
