046c214f2fc6c9533e69984c368f21f3f58d8be5
Andreas Kvernhaug <kvernhaug@hotmail.no>
2024-11-20 15:14:24 +0100

    feat: new backgrounds

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -26,16 +26,16 @@ export default function ThemeSwitcher() {
     <div>
       <div>
         <button className="m-5" onClick={() => changeTheme('spring')} aria-label="Spring Theme">
-          <Image src={'/images/spring.png'} alt="Spring Icon" width={80} height={80} />
+          <Image src={'/images/theme-icons/spring.png'} alt="Spring Icon" width={80} height={80} />
         </button>
         <button className="m-5" onClick={() => changeTheme('summer')} aria-label="Summer Theme">
-          <Image src={'/images/summer.png'} alt="Summer Icon" width={80} height={80} />
+          <Image src={'/images/theme-icons/summer.png'} alt="Summer Icon" width={80} height={80} />
         </button>
         <button className="m-5" onClick={() => changeTheme('autumn')} aria-label="Autumn Theme">
-          <Image src={'/images/autumn.png'} alt="Autumn Icon" width={80} height={80} />
+          <Image src={'/images/theme-icons/autumn.png'} alt="Autumn Icon" width={80} height={80} />
         </button>
         <button className="m-5" onClick={() => changeTheme('winter')} aria-label="Winter Theme">
-          <Image src={'/images/winter.png'} alt="Winter Icon" width={80} height={80} />
+          <Image src={'/images/theme-icons/winter.png'} alt="Winter Icon" width={80} height={80} />
         </button>
       </div>
       {theme === 'winter' && <SnowEffect />}
